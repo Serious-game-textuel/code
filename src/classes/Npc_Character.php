ba0b88096c7d80d45622dcbd6bf74ee87fa2be55
Grandhomme Paul <paul.grandhomme@hotmail.com>
2024-03-04 14:58:31 +0100

    Updated Npc character

--- src/classes/Npc_Character.php
+++ src/classes/Npc_Character.php
@@ -17,4 +17,28 @@ defined('MOODLE_INTERNAL') || die();
 global $CFG;
 require_once($CFG->dirroot . '/mod/serioustextualgame/src/classes/Character.php');
 class Npc_Character extends Character {
+
+    private int $id;
+
+    public function __construct(?int $id, string $description, string $name,
+    array $status, array $items, ?Location_Interface $currentlocation) {
+        if (!isset($id)) {
+            $super = new Character(null, $description, $name, $status, $items, $currentlocation);
+            parent::__construct($super->get_id(), "", "", [], [], null);
+            global $DB;
+            $this->id = $DB->insert_record('npccharacter', [
+                'character' => $super->get_id(),
+            ]);
+        } else {
+            $this->id = $id;
+        }
+    }
+
+    public static function get_instance(int $id) {
+        return new Npc_Character($id, "", "", [], [], null);
+    }
+
+    public function get_id() {
+        return $this->id;
+    }
 }
