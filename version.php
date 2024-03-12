0fc101358936df3323ff84867278e28f3ae8e5f4
Grandhomme Paul <paul.grandhomme@hotmail.com>
2024-03-12 15:15:20 +0100

    Updated version

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'mod_stg';
-$plugin->release = '1.0.0';
+$plugin->release = '1.1.0';
 $plugin->version = 2024013100;
 $plugin->requires = 2022112800;
 $plugin->maturity = MATURITY_ALPHA;
